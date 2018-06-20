777a5785a96c8f709055186cc56533a92a902f00
David Zhu <dyzz@google.com>
2018-06-20 16:41:11 -0700

    Make deploy scripts use absolute paths

--- deploy/kubernetes/delete-driver.sh
+++ deploy/kubernetes/delete-driver.sh
@@ -3,6 +3,10 @@
 set -o nounset
 set -o errexit
 
-kubectl delete -f node.yaml --ignore-not-found
-kubectl delete -f controller.yaml --ignore-not-found
-kubectl delete -f setup-cluster.yaml --ignore-not-found
+readonly PKGDIR="${GOPATH}/src/github.com/kubernetes-sigs/gcp-compute-persistent-disk-csi-driver"
+readonly KUBEDEPLOY="${PKGDIR}/deploy/kubernetes"
+
+kubectl delete -f "${KUBEDEPLOY}/node.yaml" --ignore-not-found
+kubectl delete -f "${KUBEDEPLOY}/controller.yaml" --ignore-not-found
+kubectl delete -f "${KUBEDEPLOY}/setup-cluster.yaml" --ignore-not-found
+kubectl delete secret cloud-sa --ignore-not-found
