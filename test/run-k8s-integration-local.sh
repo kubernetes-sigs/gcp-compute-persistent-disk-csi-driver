0f17a09331e07e7c627b591b523ceff10d9b0ee3
Michelle Au <msau@google.com>
2018-10-17 16:45:02 -0700

    Bring up driver in k8s-integration framework instead of in e2e tests. This runs deploy-driver.sh, which handles secret creation and kustomize

--- test/run-k8s-integration-local.sh
+++ test/run-k8s-integration-local.sh
@@ -4,6 +4,11 @@ set -o nounset
 set -o errexit
 
 readonly PKGDIR=${GOPATH}/src/sigs.k8s.io/gcp-compute-persistent-disk-csi-driver
+source "${PKGDIR}/deploy/common.sh"
+
+ensure_var GCE_PD_CSI_STAGING_IMAGE
+ensure_var GCE_PD_SA_DIR
 
 make -C ${PKGDIR} test-k8s-integration
-${PKGDIR}/bin/k8s-integration-test --kube-version=master --run-in-prow=false --staging-image=${GCE_PD_CSI_STAGING_IMAGE}
+${PKGDIR}/bin/k8s-integration-test --kube-version=master --run-in-prow=false --staging-image=${GCE_PD_CSI_STAGING_IMAGE} --service-account-file=${GCE_PD_SA_DIR}/cloud-sa.json --deploy-overlay-name=dev
+#${PKGDIR}/bin/k8s-integration-test --kube-version=master --run-in-prow=false --staging-image=${GCE_PD_CSI_STAGING_IMAGE} --service-account-file=${GCE_PD_SA_DIR}/cloud-sa.json --deploy-overlay-name=dev --bringup-cluster=false --teardown-cluster=false --local-k8s-dir=$KTOP
--- test/run-k8s-integration.sh
+++ test/run-k8s-integration.sh
@@ -6,4 +6,4 @@ set -o errexit
 readonly PKGDIR=${GOPATH}/src/sigs.k8s.io/gcp-compute-persistent-disk-csi-driver
 
 make -C ${PKGDIR} test-k8s-integration
-${PKGDIR}/bin/k8s-integration-test --kube-version=master --run-in-prow=true
+${PKGDIR}/bin/k8s-integration-test --kube-version=master --run-in-prow=true --deploy-overlay-name=prow-head-template --service-account-file=${E2E_GOOGLE_APPLICATION_CREDENTIALS}
