adcb623f797b2644973f85a4b9376b9dce864162
Hantao (Will) Wang <hww@google.com>
2019-06-24 11:49:59 -0700

    add ability to set cluster and kube version in integration script

--- test/run-k8s-integration-local.sh
+++ test/run-k8s-integration-local.sh
@@ -5,6 +5,8 @@ set -o errexit
 
 readonly PKGDIR=${GOPATH}/src/sigs.k8s.io/gcp-compute-persistent-disk-csi-driver
 readonly deployment_strategy=${DEPLOYMENT_STRATEGY:-gce}
+readonly gke_cluster_version=${GKE_CLUSTER_VERSION:-latest}
+readonly kube_version=${KUBE_VERSION:-master}
 
 source "${PKGDIR}/deploy/common.sh"
 
@@ -17,7 +19,8 @@ make -C ${PKGDIR} test-k8s-integration
 # --staging-image=${GCE_PD_CSI_STAGING_IMAGE} --service-account-file=${GCE_PD_SA_DIR}/cloud-sa.json \
 # --deploy-overlay-name=dev --storageclass-file=sc-standard.yaml \
 # --test-focus="External.Storage" --gce-zone="us-central1-b" \
-# --deployment-strategy=${deployment_strategy}
+# --deployment-strategy=${deployment_strategy} --gke-cluster-version=${gke_cluster_version} \
+# --kube-version=${kube_version}
 
 # This version of the command does not build the driver or K8s, points to a
 # local K8s repo to get the e2e.test binary, and does not bring up or down the cluster
@@ -26,4 +29,5 @@ ${PKGDIR}/bin/k8s-integration-test --kube-version=master --run-in-prow=false \
 --staging-image=${GCE_PD_CSI_STAGING_IMAGE} --service-account-file=${GCE_PD_SA_DIR}/cloud-sa.json \
 --deploy-overlay-name=dev --bringup-cluster=false --teardown-cluster=false --local-k8s-dir=$KTOP \
 --storageclass-file=sc-standard.yaml --do-driver-build=true  --test-focus="External.Storage" \
---gce-zone="us-central1-b"
+--gce-zone="us-central1-b" --gke-cluster-version=${gke_cluster_version} \
+--kube-version=${kube_version}
