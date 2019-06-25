2a91a29c1596038b89820ae4fbc7734915fa717e
Hantao (Will) Wang <hww@google.com>
2019-06-25 14:26:32 -0700

    break up large file into smaller pieces

---
+++ test/k8s-integration/utils.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+
+	"k8s.io/klog"
+)
+
+func runCommand(action string, cmd *exec.Cmd) error {
+	cmd.Stdout = os.Stdout
+	cmd.Stdin = os.Stdin
+	cmd.Stderr = os.Stderr
+
+	fmt.Printf("%s\n", action)
+	fmt.Printf("%s\n", cmd.Args)
+
+	err := cmd.Start()
+	if err != nil {
+		return err
+	}
+
+	err = cmd.Wait()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func shredFile(filePath string) {
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		klog.V(4).Infof("File %v was not found, skipping shredding", filePath)
+		return
+	}
+	klog.V(4).Infof("Shredding file %v", filePath)
+	out, err := exec.Command("shred", "--remove", filePath).CombinedOutput()
+	if err != nil {
+		klog.V(4).Infof("Failed to shred file %v: %v\nOutput:%v", filePath, err, out)
+	}
+	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		klog.V(4).Infof("File %v successfully shredded", filePath)
+		return
+	}
+
+	// Shred failed Try to remove the file for good meausure
+	err = os.Remove(filePath)
+	if err != nil {
+		klog.V(4).Infof("Failed to remove service account file %s: %v", filePath, err)
+	}
+}
+
+func getTempDir() string {
+	dir, err := ioutil.TempDir("", "")
+	if err != nil {
+		klog.Fatalf("Error creating temp dir: %v", err)
+	}
+	return dir
+}
+
+func removeTempDir(dir string) {
+	err := os.RemoveAll(dir)
+	if err != nil {
+		klog.Fatalf("Error removing temp dir: %v", err)
+	}
+}
+
+func ensureVariable(v *string, set bool, msgOnError string) {
+	if set && len(*v) == 0 {
+		klog.Fatal(msgOnError)
+	} else if !set && len(*v) != 0 {
+		klog.Fatal(msgOnError)
+	}
+}
+
+func ensureFlag(v *bool, setTo bool, msgOnError string) {
+	if *v != setTo {
+		klog.Fatal(msgOnError)
+	}
+}
