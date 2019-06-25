42bfce707f890a6b6761996e4a7bdafd370067af
David Zhu <dyzz@google.com>
2019-06-24 17:57:16 -0700

    Revert "Cherry-Pick: GKE Integration Support into Release-0.5.X"

--- test/run-k8s-integration-local.sh
+++ test/run-k8s-integration-local.sh
@@ -4,10 +4,6 @@ set -o nounset
 set -o errexit
 
 readonly PKGDIR=${GOPATH}/src/sigs.k8s.io/gcp-compute-persistent-disk-csi-driver
-readonly deployment_strategy=${DEPLOYMENT_STRATEGY:-gce}
-readonly gke_cluster_version=${GKE_CLUSTER_VERSION:-latest}
-readonly kube_version=${KUBE_VERSION:-master}
-
 source "${PKGDIR}/deploy/common.sh"
 
 ensure_var GCE_PD_CSI_STAGING_IMAGE
@@ -18,9 +14,7 @@ make -C ${PKGDIR} test-k8s-integration
 # ${PKGDIR}/bin/k8s-integration-test --kube-version=master --run-in-prow=false \
 # --staging-image=${GCE_PD_CSI_STAGING_IMAGE} --service-account-file=${GCE_PD_SA_DIR}/cloud-sa.json \
 # --deploy-overlay-name=dev --storageclass-file=sc-standard.yaml \
-# --test-focus="External.Storage" --gce-zone="us-central1-b" \
-# --deployment-strategy=${deployment_strategy} --gke-cluster-version=${gke_cluster_version} \
-# --kube-version=${kube_version}
+# --test-focus="External.Storage" --gce-zone="us-central1-b"
 
 # This version of the command does not build the driver or K8s, points to a
 # local K8s repo to get the e2e.test binary, and does not bring up or down the cluster
@@ -29,5 +23,4 @@ ${PKGDIR}/bin/k8s-integration-test --kube-version=master --run-in-prow=false \
 --staging-image=${GCE_PD_CSI_STAGING_IMAGE} --service-account-file=${GCE_PD_SA_DIR}/cloud-sa.json \
 --deploy-overlay-name=dev --bringup-cluster=false --teardown-cluster=false --local-k8s-dir=$KTOP \
 --storageclass-file=sc-standard.yaml --do-driver-build=true  --test-focus="External.Storage" \
---gce-zone="us-central1-b" --gke-cluster-version=${gke_cluster_version} \
---kube-version=${kube_version}
+--gce-zone="us-central1-b"
