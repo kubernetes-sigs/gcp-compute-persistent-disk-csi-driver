430f29101b22abf53c65b29b5c2408e2d8e1436c
Hantao (Will) Wang <hww@google.com>
2019-06-25 18:20:47 -0700

    add test-version flag and separate building k8s from cluster up

--- test/run-k8s-integration-local.sh
+++ test/run-k8s-integration-local.sh
@@ -4,7 +4,9 @@ set -o nounset
 set -o errexit
 
 readonly PKGDIR=${GOPATH}/src/sigs.k8s.io/gcp-compute-persistent-disk-csi-driver
-readonly deployment_strategy=${DEPLOYMENT_STRATEGY:-gce}
+readonly gke_cluster_version=${GKE_CLUSTER_VERSION:-latest}
+readonly kube_version=${KUBE_VERSION:-master}
+readonly test_version=${TEST_VERSION:-master}
 
 source "${PKGDIR}/deploy/common.sh"
 
@@ -13,15 +15,29 @@ ensure_var GCE_PD_SA_DIR
 
 make -C ${PKGDIR} test-k8s-integration
 
-# ${PKGDIR}/bin/k8s-integration-test --kube-version=master --run-in-prow=false \
+# This version of the command creates a GKE cluster. It also downloads and builds a k8s release
+# so that it can run the test specified
+
+# ${PKGDIR}/bin/k8s-integration-test --run-in-prow=false \
 # --staging-image=${GCE_PD_CSI_STAGING_IMAGE} --service-account-file=${GCE_PD_SA_DIR}/cloud-sa.json \
 # --deploy-overlay-name=dev --storageclass-file=sc-standard.yaml \
 # --test-focus="External.Storage" --gce-zone="us-central1-b" \
-# --deployment-strategy=${deployment_strategy}
+# --deployment-strategy=gke --gke-cluster-version=${gke_cluster_version} \
+# --test-version=${test_version}
+
+# This version of the command creates a GCE cluster. It downloads and builds two k8s releases,
+# one for the cluster and one for the tests, unless the cluster and test versioning is the same.
+
+# ${PKGDIR}/bin/k8s-integration-test --run-in-prow=false \
+# --staging-image=${GCE_PD_CSI_STAGING_IMAGE} --service-account-file=${GCE_PD_SA_DIR}/cloud-sa.json \
+# --deploy-overlay-name=dev --storageclass-file=sc-standard.yaml \
+# --test-focus="External.Storage" --gce-zone="us-central1-b" \
+# --deployment-strategy=gce --kube-version=${kube_version} \
+# --test-version=${test_version}
 
 # This version of the command does not build the driver or K8s, points to a
 # local K8s repo to get the e2e.test binary, and does not bring up or down the cluster
-#
+
 ${PKGDIR}/bin/k8s-integration-test --kube-version=master --run-in-prow=false \
 --staging-image=${GCE_PD_CSI_STAGING_IMAGE} --service-account-file=${GCE_PD_SA_DIR}/cloud-sa.json \
 --deploy-overlay-name=dev --bringup-cluster=false --teardown-cluster=false --local-k8s-dir=$KTOP \
