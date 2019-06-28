2de248cffabf08f9ea9dd794ec204c90ebfc56d5
David Zhu <dyzz@google.com>
2019-06-28 14:04:53 -0700

    Use new deployment strategy and version flags for k8s integration migration tests

--- test/run-k8s-integration-migration.sh
+++ test/run-k8s-integration-migration.sh
@@ -9,12 +9,19 @@
 set -o nounset
 set -o errexit
 
+export GCE_PD_VERBOSITY=9
+
 readonly PKGDIR=${GOPATH}/src/sigs.k8s.io/gcp-compute-persistent-disk-csi-driver
+
 readonly overlay_name="${GCE_PD_OVERLAY_NAME:-stable}"
 readonly boskos_resource_type="${GCE_PD_BOSKOS_RESOURCE_TYPE:-gce-project}"
 readonly do_driver_build="${GCE_PD_DO_DRIVER_BUILD:-true}"
-export GCE_PD_VERBOSITY=9
+readonly deployment_strategy=${DEPLOYMENT_STRATEGY:-gce}
+readonly kube_version=${GCE_PD_KUBE_VERSION:-master}
+readonly test_version=${TEST_VERSION:-master}
+
 readonly GCE_PD_TEST_FOCUS="\s[V|v]olume\sexpand|\[sig-storage\]\sIn-tree\sVolumes\s\[Driver:\sgcepd\]\s\[Testpattern:\sDynamic\sPV|allowedTopologies|Pod\sDisks|PersistentVolumes\sDefault"
+
 
 # TODO(#167): Enable reconstructions tests
 # TODO: Enabled inline volume tests
@@ -26,9 +33,9 @@ readonly GCE_PD_TEST_FOCUS="\s[V|v]olume\sexpand|\[sig-storage\]\sIn-tree\sVolum
 # ("us-central1-b"), but disk name is empty"
 
 make -C ${PKGDIR} test-k8s-integration
-${PKGDIR}/bin/k8s-integration-test --kube-version=${GCE_PD_KUBE_VERSION:-master} \
+${PKGDIR}/bin/k8s-integration-test --kube-version=${kube_version} \
 --kube-feature-gates="CSIMigration=true,CSIMigrationGCE=true" --run-in-prow=true \
 --deploy-overlay-name=${overlay_name} --service-account-file=${E2E_GOOGLE_APPLICATION_CREDENTIALS} \
 --do-driver-build=${do_driver_build} --boskos-resource-type=${boskos_resource_type} \
 --migration-test=true --test-focus=${GCE_PD_TEST_FOCUS} \
---gce-zone="us-central1-b"
+--gce-zone="us-central1-b" --deployment-strategy=${deployment_strategy} --test-version=${test_version}
