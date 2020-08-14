ea2d2630316c62212390416f89e93e4ccd59ea28
Matthew Cary <mattcary@google.com>
2020-08-14 08:37:20 -0700

    Add CI test for all storage classes, only test standard on pull

--- test/run-windows-k8s-integration.sh
+++ test/run-windows-k8s-integration.sh
@@ -23,7 +23,7 @@ base_cmd="${PKGDIR}/bin/k8s-integration-test \
             --platform=windows --bringup-cluster=false --teardown-cluster=false --teardown-driver=${teardown_driver}\
             --run-in-prow=true --deploy-overlay-name=${overlay_name} --service-account-file=${E2E_GOOGLE_APPLICATION_CREDENTIALS} \
             --do-driver-build=${do_driver_build} --gce-zone=${gce_zone} --test-version=${test_version}\
-            --storageclass-file=sc-windows.yaml --snapshotclass-file=pd-volumesnapshotclass.yaml --test-focus="External.Storage" \
+            --storageclass-files=sc-windows.yaml --snapshotclass-file=pd-volumesnapshotclass.yaml --test-focus="External.Storage" \
             --deployment-strategy=${deployment_strategy}"
 
 eval $base_cmd
