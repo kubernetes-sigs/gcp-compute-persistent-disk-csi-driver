12752695cc75f865d8154a42b1a062a8f72a1106
Matthew Cary <mattcary@google.com>
2021-08-31 13:02:29 -0700

    Hack kube version for presubmit
    Change-Id: I64b88fdb6eda5151a34403db692b6444c36e2a22

--- deploy/kubernetes/install-kustomize.sh
+++ deploy/kubernetes/install-kustomize.sh
@@ -17,5 +17,5 @@ if [ -f "kustomize" ]; then
   rm kustomize
 fi
 echo "installing latest version of kustomize"
-curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash
+curl -s "https://raw.githubusercontent.com/kubernetes-sigs/kustomize/master/hack/install_kustomize.sh"  | bash -x
 mv kustomize ${INSTALL_DIR}
