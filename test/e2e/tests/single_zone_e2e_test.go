b0c649a32a6d67def770afb8e5528e9b275cff01
Julianne DeMars <judemars@google.com>
2022-11-16 06:17:29 +0000

    add substring test for invalid endpoint test

--- test/e2e/tests/single_zone_e2e_test.go
+++ test/e2e/tests/single_zone_e2e_test.go
@@ -1147,7 +1147,7 @@ var _ = Describe("GCE PD CSI Driver", func() {
 		}
 
 		_, err = tcInvalid.Client.ListVolumes()
-		Expect(err).ToNot(BeNil(), "expected error when passed invalid compute url")
+		Expect(err.Error()).To(ContainSubstring("no such host"), "expected error when passed invalid compute url")
 
 		// Create new driver and client w/ valid, passed-in endpoint
 		tcValid, err := testutils.GCEClientAndDriverSetup(i, "https://compute.googleapis.com/compute/v1/")
