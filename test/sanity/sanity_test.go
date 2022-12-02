6c67b763e2377c47230d92af227223cfff10087c
Julianne DeMars <julianne.demars@gmail.com>
2022-12-01 17:19:54 -0800

    Disable per volume backoff behavior for sanity tests (#1094)
    * Lower threshold of backoff for sanity tests
    
    * Switch to zero
    
    * Rm millisecond

--- test/sanity/sanity_test.go
+++ test/sanity/sanity_test.go
@@ -65,7 +65,7 @@ func TestSanity(t *testing.T) {
 
 	//Initialize GCE Driver
 	identityServer := driver.NewIdentityServer(gceDriver)
-	controllerServer := driver.NewControllerServer(gceDriver, cloudProvider, 1*time.Millisecond, 5*time.Minute)
+	controllerServer := driver.NewControllerServer(gceDriver, cloudProvider, 0, 5*time.Minute)
 	nodeServer := driver.NewNodeServer(gceDriver, mounter, deviceUtils, metadataservice.NewFakeService(), mountmanager.NewFakeStatter(mounter))
 	err = gceDriver.SetupGCEDriver(driverName, vendorVersion, extraLabels, identityServer, controllerServer, nodeServer)
 	if err != nil {
