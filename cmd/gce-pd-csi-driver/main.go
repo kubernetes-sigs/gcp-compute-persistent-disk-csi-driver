db70a823a55889458821124193ae0060fcc82419
Sneha Aradhey <snehaaradhey@google.com>
2023-05-22 21:12:01 +0000

    removed hyperdisk driver override, updated error messages and other pr suggestions

--- cmd/gce-pd-csi-driver/main.go
+++ cmd/gce-pd-csi-driver/main.go
@@ -97,7 +97,7 @@ func handle() {
 	if *runControllerService && *httpEndpoint != "" {
 		mm := metrics.NewMetricsManager()
 		mm.InitializeHttpHandler(*httpEndpoint, *metricsPath)
-		mm.RegisterHyperdiskMetric()
+		mm.RegisterPDCSIMetric()
 
 		if metrics.IsGKEComponentVersionAvailable() {
 			mm.EmitGKEComponentVersion()
