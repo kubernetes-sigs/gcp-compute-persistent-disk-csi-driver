97ee9511cb1afcb101c2f8f57771f0462deef8b6
Max Kimambo <max@kimambo.de>
2024-06-14 12:17:26 +0000

    modify update to remove regional disk check

--- pkg/gce-cloud-provider/compute/gce-compute.go
+++ pkg/gce-cloud-provider/compute/gce-compute.go
@@ -446,15 +446,9 @@ func (cloud *CloudProvider) InsertDisk(ctx context.Context, project string, volK
 func (cloud *CloudProvider) UpdateDisk(ctx context.Context, project string, volKey *meta.Key, existingDisk *CloudDisk, params common.ModifyVolumeParameters) error {
 
 	klog.V(5).Infof("Updating disk %v", volKey)
-
-	switch volKey.Type() {
-	case meta.Zonal:
-		return cloud.updateZonalDisk(ctx, project, volKey, existingDisk, params)
-	case meta.Regional:
-		return cloud.updateRegionalDisk(ctx, project, volKey, existingDisk, params)
-	default:
-		return fmt.Errorf("could not update disk, key was neither zonal nor regional, instead got: %v", volKey.String())
-	}
+	// hyperdisks are zonal disks
+	// pd-disks do not support modification of IOPS and Throughput
+	return cloud.updateZonalDisk(ctx, project, volKey, existingDisk, params)
 }
 
 func (cloud *CloudProvider) updateZonalDisk(ctx context.Context, project string, volKey *meta.Key, existingDisk *CloudDisk, params common.ModifyVolumeParameters) error {
@@ -475,25 +469,6 @@ func (cloud *CloudProvider) updateZonalDisk(ctx context.Context, project string,
 
 	fmt.Printf("http status : %d", updateOpResult.HTTPStatusCode)
 
-	return nil
-}
-
-func (cloud *CloudProvider) updateRegionalDisk(ctx context.Context, project string, volKey *meta.Key, existingDisk *CloudDisk, params common.ModifyVolumeParameters) error {
-
-	updatedDisk := &computev1.Disk{
-		Name:                  existingDisk.GetName(),
-		ProvisionedIops:       params.IOPS,
-		ProvisionedThroughput: params.Throughput,
-	}
-
-	diskUpdateOp := cloud.service.RegionDisks.Update(project, volKey.Region, volKey.Name, updatedDisk)
-	diskUpdateOp.Paths("provisionedIops", "provisionedThroughput")
-	updateOpResult, err := diskUpdateOp.Context(ctx).Do()
-
-	if err != nil {
-		return fmt.Errorf("error updating disk %v: %w", volKey, err)
-	}
-	fmt.Printf("http status : %d", updateOpResult.HTTPStatusCode)
 	return nil
 }
 
