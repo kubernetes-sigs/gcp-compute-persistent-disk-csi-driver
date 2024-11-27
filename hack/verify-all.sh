4249ee3fe376242294ce975da2c6187424660b7d
Matthew Cary <mattcary@google.com>
2024-11-27 11:27:22 -0800

    Remove make from verify-all in order to reduce presubmit flakes

--- hack/verify-all.sh
+++ hack/verify-all.sh
@@ -23,5 +23,3 @@ PKG_ROOT=$(git rev-parse --show-toplevel)
 "${PKG_ROOT}/hack/verify-gofmt.sh"
 "${PKG_ROOT}/hack/verify-govet.sh"
 "${PKG_ROOT}/hack/verify-docker-deps.sh"
-
-make -C "${PKG_ROOT}" all
