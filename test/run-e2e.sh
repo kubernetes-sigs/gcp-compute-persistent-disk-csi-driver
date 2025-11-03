2fa33f0d38cc9a5ae699f3dd9e30a06fce500ddf
Sunny Song <songsunny@google.com>
2025-11-03 21:12:37 +0000

    Increase e2e tests timeout for ARM tests

--- test/run-e2e.sh
+++ test/run-e2e.sh
@@ -5,9 +5,6 @@ set -x
 
 readonly PKGDIR=sigs.k8s.io/gcp-compute-persistent-disk-csi-driver
 
-TIMEOUT=50m
-if [ "$RUN_CONTROLLER_MODIFY_VOLUME_TESTS" = true ]; then
-    TIMEOUT=45m
-fi
+TIMEOUT=60m
 
 go test --timeout "${TIMEOUT}" --v "${PKGDIR}/test/e2e/tests" --run-in-prow=true --delete-instances=true --logtostderr $@
