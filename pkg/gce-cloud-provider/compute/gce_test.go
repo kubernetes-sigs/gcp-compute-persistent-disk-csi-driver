552bada10be124599436755fe942d4a54b713133
Patrick Dillon <padillon@redhat.com>
2026-07-16 17:53:54 -0400

    Add Universe Domain Support
    Adds custom universe domain support in order to allow operation
    in environments with non-default universe domains, such as
    Google Cloud Dedicated, GCP's sovereign cloud offering.
    
    The universe domain can be determined from the credential, and
    replaces the standard googleapis.com resource path.

--- pkg/gce-cloud-provider/compute/gce_test.go
+++ pkg/gce-cloud-provider/compute/gce_test.go
@@ -202,7 +202,7 @@ func TestGetComputeVersion(t *testing.T) {
 	}
 	for _, tc := range testCases {
 		ctx := context.Background()
-		computeOpts, err := getComputeVersion(ctx, &mockTokenSource{}, tc.computeEndpoint, tc.computeEnvironment, tc.computeVersion, 0)
+		computeOpts, err := getComputeVersion(ctx, nil, &mockTokenSource{}, tc.computeEndpoint, tc.computeEnvironment, tc.computeVersion, 0, "")
 		service, _ := compute.NewService(ctx, computeOpts...)
 		gotEndpoint := service.BasePath
 		if err != nil && !tc.expectError {
