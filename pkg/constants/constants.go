1f50cad7d018a353707d5de973786595ecfa72a1
sudhirshanmugam <sudhir.shanmugam@techmahindra.com>
2026-09-08 15:54:55 +0530

    Add the disk type parameter to VolumeAttributesClass

--- pkg/constants/constants.go
+++ pkg/constants/constants.go
@@ -87,6 +87,28 @@ const (
 
 	// ClusterIDLabel is the key in disk labels to indicate the cluster identifier of the disk.
 	ClusterIDLabel = "cluster-id-gke-io"
+
+	// Annotations for Disk Type Conversion
+	DiskTypeConversionOperationKey = "pdcsi.gke.io/disk-type-conversion-operation"
+	DiskTypeConvertedFromKey       = "pdcsi.gke.io/disk-type-converted-from"
+	DiskTypeConvertedToKey         = "pdcsi.gke.io/disk-type-converted-to"
+	// DiskTypeConversionLastErrorKey holds the error reported by a conversion
+	// that ran and failed but will be retried, so that the failure can be
+	// passed through to the claim on the next attempt rather than being visible
+	// only as an event.
+	DiskTypeConversionLastErrorKey = "pdcsi.gke.io/disk-type-conversion-last-error"
+
+	// Conversion States
+	ConversionStatePending = "Pending"
+
+	// Reasons and actions for the events emitted while converting a disk type.
+	DiskTypeConversionStartReason    = "DiskTypeConversionStart"
+	DiskTypeConversionCompleteReason = "DiskTypeConversionComplete"
+	DiskTypeConversionRetryReason    = "DiskTypeConversionRetry"
+	DiskTypeConversionCancelReason   = "DiskTypeConversionCancelled"
+	DiskTypeConversionQueuedReason   = "DiskTypeConversionQueued"
+	DiskTypeConversionFailedReason   = "DiskTypeConversionFailed"
+	DiskTypeConversionAction         = "ConvertDiskType"
 )
 
 // doc https://cloud.google.com/compute/docs/general-purpose-machines
